d45387c5df1a188fb6d76ad7d0db36db90079057
Andrew Turner <andrewturner100@hotmail.co.uk>
2019-03-07 22:16:47 +0000

    readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Beer Route
+
+## How to run
+
+### Preprocessing
+
+1. Run `python make_breweries_csv.py` to generate `beer_counts.csv`.
+2. Run `python make_distance_matrix.py` to generate `distances.csv`.
+
+### Finding the route
+
+1. Run `python main.py`.
---
+++ main.py
@@ -0,0 +1,114 @@
+import numpy
+import pandas
+from haversine import haversine
+
+class Brewery:
+    def __init__(self, brewery_id, name, beers_count, location, distances_index):
+        self.brewery_id = brewery_id
+        self.name = name
+        self.beers_count = beers_count
+        self.location = location
+        self.distances_index = distances_index
+
+    def __str__(self):
+        return self.name
+
+
+class Journey:
+    def __init__(self, home):
+        self.home = home
+
+        self.breweries = []
+        self.final_distance = 0
+    
+    def record_brewery(self, brewery, distance):
+        self.breweries.append((brewery, distance))
+
+    def record_home(self, final_distance):
+        self.final_distance = final_distance
+
+    def score(self):
+        return sum(brewery.beers_count for brewery, distance in self.breweries)
+
+    def print_out(self):
+        print(f"Found {len(self.breweries)} beer factories:")
+        print(f"    -> HOME: {self.home} distance 0km")
+        beers_collected = 0
+        total_distance = 0
+        for brewery, distance in self.breweries:
+            beers_collected += brewery.beers_count
+            total_distance += distance
+            print(f"    -> [{brewery.brewery_id}] {brewery.name}: {brewery.location} distance {distance:.0f}km")
+        print(f"    <- HOME: {self.home} distance {self.final_distance:.0f}km")
+        total_distance += self.final_distance
+        print(f"Total distance travelled: {total_distance:.0f}km")
+        print(f"Collected {beers_collected} beer types")
+
+
+
+beer_counts = pandas.read_csv("beer_counts.csv")
+distances = numpy.loadtxt("distances.csv")
+
+breweries = [Brewery(row['brewery_id'], row['name'], row['beers_count'], (row['latitude'], row['longitude']), i) for i, row in beer_counts.iterrows()]
+
+def find_journey(home_location, scan):
+    home_distances = [
+        haversine(home_location, (row['latitude'], row['longitude']))
+        for i, row in beer_counts.iterrows()
+    ]
+
+    current_fuel = 2000
+    beers_collected = 0
+    visited = set()
+
+    journey = Journey(home_location)
+
+    available_breweries = sorted(breweries, key=lambda b: home_distances[b.distances_index])[:scan]
+    selected_brewery = max(available_breweries, key=lambda b: b.beers_count)
+    selected_distance = home_distances[selected_brewery.distances_index]
+
+    current_fuel -= selected_distance
+    beers_collected += selected_brewery.beers_count
+    visited.add(selected_brewery)
+
+    journey.record_brewery(selected_brewery, selected_distance)
+
+    while current_fuel > 0:
+        previous_brewery = selected_brewery
+        available_breweries = filter(lambda b: b not in visited and home_distances[b.distances_index] < current_fuel - distances[previous_brewery.distances_index, b.distances_index], breweries)
+        candidates = sorted(available_breweries, key=lambda b: distances[previous_brewery.distances_index, b.distances_index])[:scan]
+        
+        if not candidates:
+            break
+        
+        selected_brewery = max(candidates, key=lambda b: b.beers_count)
+        selected_distance = distances[previous_brewery.distances_index, selected_brewery.distances_index]
+
+        current_fuel -= selected_distance
+        beers_collected += selected_brewery.beers_count
+        visited.add(selected_brewery)
+
+        journey.record_brewery(selected_brewery, selected_distance)
+
+    journey.record_home(home_distances[selected_brewery.distances_index])
+
+    return journey
+
+if __name__ == "__main__":
+    home_location = (51.355468, 11.100790)
+    
+    best_journey = None
+    best_score = 0
+    best_scan = None
+    for i in range(1, 20):
+        journey = find_journey(home_location, i)
+        if journey.score() > best_score:
+            best_journey = journey
+            best_score = journey.score()
+            best_scan = i
+
+    print(f"Scan {best_scan}")
+    print("")
+    best_journey.print_out()
+        
+    
